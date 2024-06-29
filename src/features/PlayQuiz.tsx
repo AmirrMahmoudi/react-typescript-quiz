6b90170ea6c90a43318fc046c12dfacd2c303113
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-06-29 11:30:28 +0330

    Displaying questions and answers

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -1,8 +1,47 @@
+import { useState } from "react";
 import { QuizItem } from "../types/quiz-type";
+import {
+  Flex,
+  Heading,
+  Radio,
+  RadioGroup,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 
 const PlayQuiz = (p: { quiz: QuizItem[] }) => {
-  console.log(p.quiz);
-  return <>Play</>;
+  const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
+  const currentQuizItem: QuizItem = p.quiz[currentQuizItemIndex];
+
+  const availableAnswers: string[] = [
+    currentQuizItem.correct_answer,
+    ...currentQuizItem.incorrect_answers,
+  ];
+  const radioList = availableAnswers.map((availableAnswer: string) => (
+    <Radio key={availableAnswer} value={availableAnswer}>
+      <Text dangerouslySetInnerHTML={{ __html: availableAnswer }}></Text>
+    </Radio>
+  ));
+
+  return (
+    <Flex direction={"column"} alignItems={"center"} justify={"center"}>
+      <Heading
+        fontSize={"3xl"}
+        mt={100}
+        mb={20}
+        dangerouslySetInnerHTML={{ __html: currentQuizItem.question }}
+      />
+
+      <RadioGroup
+        value={""}
+        onChange={() => setCurrentQuizItemIndex(currentQuizItemIndex + 1)}
+      >
+        <SimpleGrid columns={2} spacing={4}>
+          {radioList}
+        </SimpleGrid>
+      </RadioGroup>
+    </Flex>
+  );
 };
 
 export default PlayQuiz;
